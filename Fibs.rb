dcf11fb7395ae00f08d0ea12888d175c399810ce
Paul Houle <pch247@gmail.com>
2014-05-19 19:36:06 -0400

    cleaned up code

--- Fibs.rb
+++ Fibs.rb
@@ -3,10 +3,12 @@ current_number, next_number = 0,1
 
 	(1000).times do unless next_number > 1000
 	    previous_number = current_number 
-	    current_number, next_number = next_number, current_number + next_number	   
+	    current_number, next_number = next_number, current_number + next_number	
+	    	        
 	        unless next_number > 1000
 	   		    fibonacci_numbers << 
 	   		    "F(#{previous_number},#{current_number}) = #{next_number}"
+	   		    
 	   		    if previous_number == 0
 	   		    	fibonacci_numbers << 
 	   		        "F(#{previous_number},#{current_number}) = #{next_number}"
