b5b2bb0211eefb77c616f981ea475a88f2d7091d
Paul Houle <pch247@gmail.com>
2014-05-20 09:18:07 -0400

    fibonacci number exercise

--- Fibs.rb
+++ Fibs.rb
@@ -3,19 +3,18 @@ current_number, next_number,fibonacci_number = 0,1,0
 
 	(1000).times do unless next_number > 1000
 	    
-	    previous_number = current_number
+	    	if current_number == 0
+	       		fibonacci_number += 1
+	       		fibonacci_numbers << 
+	        	"F(#{fibonacci_number}) = #{next_number}"
+	   		end	   	
+	    
 	    current_number, next_number = next_number, current_number + next_number	
 
-	        unless next_number > 1000
+	    	unless next_number > 1000
 	        	fibonacci_number += 1
 	   		    fibonacci_numbers << 
-	   		    "F(#{fibonacci_number}) = #{next_number}"
-	   		    
-	   		    if previous_number == 0
-	   		    	fibonacci_number += 1
-	   		    	fibonacci_numbers << 
-	   		        "F(#{fibonacci_number}) = #{next_number}"
-	   		    end	   		   
+	   		    "F(#{fibonacci_number}) = #{next_number}"	   		    	   
 	        end
 	    end  
 	end
