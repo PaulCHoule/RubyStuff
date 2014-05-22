da43afff8778b9b9e694540f897e3e945884daf0
Paul Houle <pch247@gmail.com>
2014-05-22 11:12:40 -0400

    prime factor function

--- PrimeFactor.rb
+++ PrimeFactor.rb
@@ -1,16 +1,18 @@
-number_to_factor = 600851475143
-divisor = 2
+def prime_factor(number_to_factor)
+  divisor = 2
+  until divisor > number_to_factor do
 
-until divisor > number_to_factor do
-
-  if number_to_factor % divisor == 0
-    largest_prime_factor = divisor
-    number_to_factor = number_to_factor/divisor
-    divisor = 2
-  else
-    divisor += 1
+    if number_to_factor % divisor == 0
+      largest_prime_factor = divisor
+      number_to_factor = number_to_factor/divisor
+      divisor = 2
+    else
+      divisor += 1
+    end
+    
   end
-  
+
+  return largest_prime_factor
 end
 
-puts largest_prime_factor
+puts prime_factor(600851475143)
